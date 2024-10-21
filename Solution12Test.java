ee59bc0fcba546586d2b50fa8b9c3507f89417ac
wys <1452289627@qq.com>
2024-10-21 13:32:10 +0800

    编写测试文件

---
+++ Solution12Test.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution12Test {
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+        Solution12 test=new Solution12();
+        assertEquals("6",test.multiply("2","3"));
+        assertEquals("18",test.multiply("6","3"));
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void addStrings() {
+
+    }
+}
